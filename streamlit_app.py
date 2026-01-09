7d0e0592d88fd0e18bde18c8350a5ca00046c927
Teslim Lawal <95858261+Teslim53@users.noreply.github.com>
2026-01-09 10:16:40 -0500

    Fix API request URL and comment out SQL insert
    Fixed URL formatting for API request and commented out SQL insert statement.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,15 +31,15 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string+= fruit_chosen + ' '
         st.subheader(fruit_chosen+'Nutirition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/ "+fruit_chosen)
         #  sf_df, because it's our SmoothieFroot Dataframe.
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
         
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
+    # my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+    #         values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
     
     st.write(my_insert_stmt)
     # st.stop()
